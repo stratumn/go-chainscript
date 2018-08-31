7f062f9dd62830ff653d37c59f4578839cfffcd8
t-bast <bastien@stratumn.com>
2018-08-31 12:09:55 +0200

    cmd: add CLI to run compatibility tests

---
+++ cmd/testcase.go
@@ -0,0 +1,29 @@
+// Copyright 2017-2018 Stratumn SAS. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+// TestCase to run accross ChainScript implementations.
+type TestCase interface {
+	// Generate encoded segment bytes.
+	Generate() string
+	// Validate encoded segment bytes.
+	Validate(string) error
+}
+
+// TestData associated to a given test.
+type TestData struct {
+	ID   string `json:"id"`
+	Data string `json:"data"`
+}
